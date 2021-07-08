ea6702a2a88a7ab8aaaf9a29de2c916c1bc97625
fedir72 <fedir72@ua.fm>
2021-07-08 14:24:29 +0300

    done task

---
+++ testAppGestures/Pods/Target Support Files/Pods-testAppGestures/Pods-testAppGestures-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_testAppGestures : NSObject
+@end
+@implementation PodsDummy_Pods_testAppGestures
+@end
--- testAppGestures/testAppGestures/ViewController.swift
+++ testAppGestures/testAppGestures/ViewController.swift
@@ -6,14 +6,102 @@
 //
 
 import UIKit
+import SnapKit
 
 class ViewController: UIViewController {
-
+    
+    //MARK: - properties
+    
+    private var viewRotation: CGFloat = 0
+    private var scaleX: CGFloat = 1
+    private var scaleY: CGFloat = 1
+    
+    private let colours:[UIColor] = [.red,.yellow,.green,.orange]
+    
+    var indexer: Int = 0 {
+        didSet { if indexer == colours.count {indexer = 0} }
+    }
+    
+    //MARK: - outlets
+    
+    @IBOutlet weak var blueView: UIView!
+    
+    //MARK: - viewDidLoad()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
     }
+    
+    
+    //MARK: - actions
+    
+   
+    
+    //MARK: - rotating
+    @IBAction func rotationblueViewGesture(_ sender: UIRotationGestureRecognizer) {
+        if let obj = sender.view {
+            obj.transform = obj.transform.rotated(by: sender.rotation) //поворот
+            viewRotation += sender.rotation
+            print(viewRotation)
+            sender.rotation = 0 //нынешнее положение как начальное
+        }
+    }
+    
+    
+    //MARK: - single tap
+    @IBAction func changeColorGesture(_ sender: UITapGestureRecognizer) {
+        print("tapgesture")
+        blueView.backgroundColor = getnextColor()
+    }
+    
+    //MARK: - slide view
+    @IBAction func moveVieGesture(_ sender: UIPanGestureRecognizer) {
+        
+        let transition = sender.translation(in: self.view)
+        
+        if let senderView = sender.view {
+            senderView.center = CGPoint(x: senderView.center.x + transition.x,
+                                       y: senderView.center.y + transition.y)
+        }
+     
+        sender.setTranslation(CGPoint.zero, in: self.view)
+        
+    }
+    @IBAction func pinchGestureBluView(_ sender: UIPinchGestureRecognizer) {
+        
+        if let obj = sender.view {
+            
+            self.scaleX = sender.scale
+            self.scaleY = sender.scale
+            
+            obj.transform = obj.transform.scaledBy(x: sender.scale, y: sender.scale)
+            print(obj.transform.scaledBy(x: sender.scale, y: sender.scale))
+            
+            sender.scale = 1
+        }
+    }
+    //MARK: tap to mainView
+    @IBAction func didtapMainView(_ sender: UITapGestureRecognizer) {
+  
+        UIView.animate(withDuration: 0.5) {
+            self.blueView.backgroundColor = .systemBlue
+            self.blueView.center = self.view.center
+            self.blueView.transform = .init(rotationAngle: self.viewRotation)
+            self.blueView.transform = .init(scaleX: self.scaleX , y: self.scaleY)
+        }
+    }
+}
 
+//MARK: - methods
 
+private extension ViewController {
+    
+    func getnextColor() -> UIColor {
+        let color = colours[indexer]
+        indexer+=1
+        return color
+        
+    }
 }
 
